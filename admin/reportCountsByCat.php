72595ff200b69d8c268de7d937b78b2c707d8189
TechnoMageUU <32242832+TechnoMageUU@users.noreply.github.com>
2017-11-16 17:40:02 -0500

    Thursday's Update

---
+++ admin/reportCountsByCat.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<?php
+    include './classDB.php';
+    include './classDBUtilities.php';
+    include './classCats.php';
+    $cats = new classCats();
+    $searchResults = $cats->getCountsByCategory();
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Item Count by Category</title>
+        <link rel='stylesheet' type='text/css' href='biblioUU.css'>        
+    </head>
+    <body>
+        <h1>Item Count by Category</h1>
+        <p>
+            This report shows the number of items in each Category.
+        </p>
+        <hr />
+        <?php
+            echo '<br />' . $searchResults;
+        ?>
+    </body>
+</html>
