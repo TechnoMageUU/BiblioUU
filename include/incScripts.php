da3f26d2caeac13f36a9dc183ac65f54751ab33b
TechnoMageUU <32242832+TechnoMageUU@users.noreply.github.com>
2017-11-16 17:41:53 -0500

    Thursday's Update

---
+++ include/incScripts.php
@@ -0,0 +1,16 @@
+<?php
+/* 
+ * Standard script and style includes
+ * Copyright 2017 Frederick CUPPS
+ */
+?>
+<script src="<?php echo BASE_URL; ?>/scripts/jquery-3.2.1.min.js"></script>
+<script src="<?php echo BASE_URL; ?>/scripts/jquery-ui.min.js"></script>
+<script src="<?php echo BASE_URL; ?>/scripts/biblioUU.js"></script>
+    
+<link rel='stylesheet' type='text/css' href='<?php echo BASE_URL; ?>/styles/jquery-ui.min.css'>
+<link rel='stylesheet' type='text/css' href='<?php echo BASE_URL; ?>/styles/jquery-ui.structure.css'>
+<link rel='stylesheet' type='text/css' href='<?php echo BASE_URL; ?>/styles/jquery-ui.theme.min.css'>
+<link rel='stylesheet' type='text/css' href='<?php echo BASE_URL; ?>/styles/biblioUU.css'>
+
+
